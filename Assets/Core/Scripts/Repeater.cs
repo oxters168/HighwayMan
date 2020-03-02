2fc686e364e2380d8c6b0c402875847b19111416
Oxters Wyzgowski <oxters168@gmail.com>
2020-03-02 02:21:48 +0200

    Imported PolygonCity. Imported AkilliMum's Standard Shaders and Post Effects asset. Imported MeshCombineStudio. Created some highway prefabs and combined a five lane road. Made highway repeat with camera. Added pixelation to camera.

---
+++ Assets/Core/Scripts/Repeater.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Repeater : MonoBehaviour
+{
+    public Transform target;
+    public Transform front, mid, back;
+
+    public float size = 50;
+    public float offset = 25;
+
+    void Update()
+    {
+        int index = Mathf.CeilToInt((target.position.z - offset) / size);
+        mid.position = new Vector3(mid.position.x, mid.position.y, index * size);
+        front.position = new Vector3(front.position.x, front.position.y, (index + 1) * size);
+        back.position = new Vector3(back.position.x, back.position.y, (index - 1) * size);
+    }
+}

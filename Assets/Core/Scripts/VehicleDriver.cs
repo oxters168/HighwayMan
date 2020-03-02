107baa23850f83d38cae86761c011209a48ec7b2
Oxters Wyzgowski <oxters168@gmail.com>
2020-03-02 17:24:54 +0200

    Imported NWH's Vehicle Physics asset (for the wheel controller, unity's sucks). Changed how force is applied to VehicleDriver to be based on speed rather than position. Added steering to VehicleDriver script.

--- Assets/Core/Scripts/VehicleDriver.cs
+++ Assets/Core/Scripts/VehicleDriver.cs
@@ -1,4 +1,6 @@
-﻿using TMPro;
+﻿using NWH.VehiclePhysics;
+using NWH.WheelController3D;
+using TMPro;
 using UnityEngine;
 using UnityHelpers;
 
@@ -8,6 +10,9 @@ public class VehicleDriver : MonoBehaviour
 
     public Rigidbody physics;
 
+    public Transform wheelFL, wheelFR;
+    public WheelController wheelRL, wheelRR;
+
     [Tooltip("In m/s^2")]
     public float acceleration = 3.4f;
     [Tooltip("In m/s^2")]
@@ -15,32 +20,49 @@ public class VehicleDriver : MonoBehaviour
     [Tooltip("In m/s^2")]
     public float brakeleration = 10f;
     [Tooltip("In m/s")]
-    public float maxSpeed = 57.2f;
+    public float maxForwardSpeed = 57.2f;
+    [Tooltip("In m/s")]
+    public float maxReverseSpeed = 28.6f;
+    [Tooltip("In degrees")]
+    public float maxWheelAngle = 33.33f;
 
     public float currentSpeed;
 
-    [Range(0, 1)]
+    [Range(-1, 1)]
     public float gas;
     [Range(0, 1)]
     public float brake;
+    [Range(-1, 1)]
+    public float steer;
 
     void FixedUpdate()
     {
-        float gasAmount = gas * acceleration * (currentSpeed >= 0 ? 1 : -1);
-        float brakeAmount = brake * brakeleration * (currentSpeed >= 0 ? -1 : 1);
-        float totalAcceleration = gasAmount + brakeAmount;
-        if (totalAcceleration > -float.Epsilon && totalAcceleration < float.Epsilon && !(currentSpeed > -float.Epsilon && currentSpeed < float.Epsilon))
-            totalAcceleration = deceleration * (currentSpeed >= 0 ? -1 : 1);
-        float deltaSpeed = totalAcceleration * Time.fixedDeltaTime;
+        Quaternion wheelRotation = Quaternion.Euler(0, maxWheelAngle * steer, 0);
+        wheelFL.localRotation = wheelRotation;
+        wheelFR.localRotation = wheelRotation;
 
-        currentSpeed = Mathf.Clamp(currentSpeed + deltaSpeed, 0, maxSpeed);
+        float forwardPercent = Mathf.Clamp(Vector3.Angle(transform.forward, physics.velocity), 0, 90) / 90;
+        float actualForwardSpeed = physics.velocity.magnitude * forwardPercent;
+        speedGauge.text = actualForwardSpeed.ToString();
 
-        float actualSpeed = physics.velocity.magnitude;
-        //if (!(currentSpeed > -float.Epsilon && currentSpeed < float.Epsilon) && actualSpeed < currentSpeed)
-        //{
-            Vector3 nextCalculatedPosition = physics.position + transform.forward * currentSpeed;
-            physics.AddForce(physics.CalculateRequiredForce(nextCalculatedPosition), ForceMode.Force);
-        //}
-        speedGauge.text = actualSpeed.ToString();
+        if (wheelRL.isGrounded && wheelRR.isGrounded)
+        {
+            gas = Mathf.Clamp(gas, -1, 1);
+            brake = Mathf.Clamp(brake, 0, 1);
+            steer = Mathf.Clamp(steer, -1, 1);
+
+            float gasAmount = gas * acceleration;
+            float brakeAmount = brake * brakeleration * (currentSpeed >= 0 ? -1 : 1);
+            float totalAcceleration = gasAmount + brakeAmount;
+            if (totalAcceleration > -float.Epsilon && totalAcceleration < float.Epsilon && !(currentSpeed > -float.Epsilon && currentSpeed < float.Epsilon))
+                totalAcceleration = deceleration * (currentSpeed >= 0 ? -1 : 1);
+            float deltaSpeed = totalAcceleration * Time.fixedDeltaTime;
+
+            currentSpeed = Mathf.Clamp(currentSpeed + deltaSpeed, -maxReverseSpeed, maxForwardSpeed);
+
+            physics.AddForce(physics.CalculateRequiredForceForSpeed(currentSpeed * transform.forward), ForceMode.Force);
+        }
+        else
+            currentSpeed = actualForwardSpeed;
     }
 }
